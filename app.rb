44afbd648a35d18541bd3999c3ffa439358086e4
Robert Faldo <robertfaldo@gmail.com>
2018-05-30 15:36:57 +0100

    Completed Sinatra forms with Angus

--- app.rb
+++ app.rb
@@ -13,11 +13,17 @@ get "/test" do
   "this prints without updating server"
 end
 
-get "/cat" do
+get "/random-cat" do
   @name = ["Amigo", "Oscar", "Viking"].sample
   erb(:index)
 end
 
 get "/testing" do
   erb "Hi there <%=2 + 2%>"
+end
+
+get "/named-cat" do
+  puts params
+  @name = params[:name]
+  erb(:index)
 end
